933b5b7b0f36b19a91b1e8b380990e0ee6996919
thersgo <90255869+thersgo@users.noreply.github.com>
2025-11-20 15:17:51 +0200

    Renamed files to be in right order, added negative check to yl09

--- python/yl09.py
+++ python/yl09.py
@@ -7,7 +7,9 @@ for i in range(3):
 sides.sort()
 (a, b, c) = sides
 
-if a + b < c:
+if a < 0 or b < 0 or c < 0:
+	print("Ükski külg ei saa negatiivse suurusega olla!")
+elif a + b < c:
 	print("See küll pole päris kolmnurk! (lühemad küljed ei ulatu kokku!)")
 elif a == b == c: # chained comparison is the only* good thing about python
 	print("See kolmnurk on võrdkülgne.")
